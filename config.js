13b83eb7fb8dcc803902ce81b370380dee153fe8
SudoAnirudh <anirudhsudheer@gmail.com>
2021-10-23 16:11:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -36,7 +36,7 @@ global.author = 'Anirudh'
 
 global.wait = '_*Please Wait..*_'
 global.eror = '_*Server Error*_'
-global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
+global.fla = 'https://ibb.co/7SmJ4T2'
 
 global.watermark = '© EVA'
 
