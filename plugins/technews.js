b7456bec9ce1cd6a4f8bda51734c445acf3848ce
SudoAnirudh <anirudhsudheer@gmail.com>
2021-10-29 07:02:21 +0530

    Eva

--- plugins/technews.js
+++ plugins/technews.js
@@ -1,10 +1,6 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
-    let url = global.API('https://www.theverge.com/rss/index.xml')
-    if (!res.ok) throw await `${res.status} ${res.statusText}`
-  let json = await res.json()
-  if (!json.status) throw json
-  m.reply(json.result)
+    const url = `https://inshortsapi.vercel.app/news?category=${match[1]}`;
 }
 handler.help = ['technews','techupdates']
 handler.tags = ['internet']
