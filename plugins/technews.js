f29571c864ef289068f01e32ed563628532c1565
SudoAnirudh <anirudhsudheer@gmail.com>
2021-10-30 06:21:54 +0530

    Eva

--- plugins/technews.js
+++ plugins/technews.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn, text }) => {
-let res = await fetch('https://www.theverge.com/rss/index.xml')
+let res = await fetch('https://feed2json.org/convert?url=https%3A%2F%2Fwww.theverge.com%2Frss%2Findex.xml')
 if (!res.ok) throw await `${res.status} ${res.statusText}`;
 }
 handler.command = /^(technews)$/i
