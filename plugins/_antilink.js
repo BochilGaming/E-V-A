06105e305ef38551d6b5a969eeab163891afc497
SudoAnirudh <anirudhsudheer@gmail.com>
2021-11-06 21:25:31 +0530

    Eva

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -6,7 +6,7 @@ module.exports = {
     let isGroupLink = linkRegex.exec(m.text)
 
     if (chat.antiLink && isGroupLink) {
-      m.reply('Hapus!!\n\nLink Grup terdeteksi')
+      m.reply('Delete!!\n\nGroup link detected')
       if (global.opts['restrict']) {
         if (isAdmin || !isBotAdmin) return true
         // this.groupRemove(m.chat, [m.sender])
