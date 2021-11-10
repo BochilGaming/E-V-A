f8136c12ee794b760a5b82def16cac639abac7d3
SudoAnirudh <anirudhsudheer@gmail.com>
2021-11-10 06:54:51 +0530

    Eva

--- plugins/ytshorts.js
+++ plugins/ytshorts.js
@@ -2,17 +2,11 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
   if (!args[0]) throw `uhm.. where is the url?\n\nExample:\n${usedPrefix + command} https://youtube.com/shorts/`
   if (!args[0].match(/shorts/gi)) throw `wrong url`
-  
-  let res = await fetch(global.API('fxc7', '/api/download/ytshort', { url: args[0] }, 'apikey'))
+  let res = await fetch(API('fxc7', '/api/download/ytshort', { url: args[0] }, 'apikey'))
   if (!res.ok) throw eror
   let json = await res.json()
-  if(!json.data[0]) throw json
-  let { title, channel, thumb, views,  } = json.data
-let ytshorts = `✨️ *Title:* ${title}
- *Channel:* ${channel}
- *Quality*: ${quality}
- *Direct URL:* ${views}\n\n@Eva`
-
+  if (!json.status) throw json        
+  await m.reply(wait)
   await conn.sendFile(m.chat, thumb, '', ytshorts, m)
   await conn.sendFile(m.chat, link, '', ytshorts, m)
 }
