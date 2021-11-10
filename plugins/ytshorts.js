f6da8fd5da9f45c859e4358161a992aeb12eac36
SudoAnirudh <anirudhsudheer@gmail.com>
2021-11-10 06:43:40 +0530

    Eva

--- plugins/ytshorts.js
+++ plugins/ytshorts.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
   if (!args[0].match(/shorts/gi)) throw `wrong url`
   
   let res = await fetch(global.API('fxc7', '/api/download/ytshort', { url: args[0] }, 'apikey'))
-  if (!res.ok) throw await res.text()
+  if (!res.ok) throw eror
   let json = await res.json()
   if(!json.data[0]) throw json
   let { title, channel, thumb, views,  } = json.data[0]
