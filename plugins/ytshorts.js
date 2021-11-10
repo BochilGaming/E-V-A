8ddf870a51b0c653bc8ac7f84176a0de6cdf2ce4
SudoAnirudh <anirudhsudheer@gmail.com>
2021-11-10 06:38:36 +0530

    Eva

--- plugins/ytshorts.js
+++ plugins/ytshorts.js
@@ -1,21 +1,23 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-
   if (!args[0]) throw `uhm.. where is the url?\n\nExample:\n${usedPrefix + command} https://youtube.com/shorts/`
   if (!args[0].match(/shorts/gi)) throw `wrong url`
-
-  let res = await fetch(API('fxc7', '/api/download/ytshort', { url: args[0] }, 'apikey'))
-  if (!res.ok) throw eror
+  
+  let res = await fetch(global.API('fxc7', '/api/download/ytshort', { url: args[0] }, 'apikey'))
+  if (!res.ok) throw await res.text()
   let json = await res.json()
-  // if (!json.status) throw json        
-  await m.reply(wait)
-  await conn.sendFile(m.chat, json.link, '', `${json.title}\n\n© Eva`, m)
+  if(!json.data[0]) throw json
+  let { title, channel, thumb, views,  } = json.data[0]
+let ytshorts = `✨️ *Title:* ${title}
+ *Channel:* ${channel}
+ *Quality*: ${quality}
+ *Direct URL:* ${views}\n\n@Eva`
 
+  await conn.sendFile(m.chat, thumb, '', ytshorts, m)
+  await conn.sendFile(m.chat, link, '', ytshorts, m)
 }
 handler.help = ['ytshorts'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 handler.command = /^(ytshorts)$/i
-
 handler.limit = true
-
 module.exports = handler
